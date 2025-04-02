8202303551f6f18d429fe72198e674148be96bf6
Lucas Machado <lucasmachadoale4@gmail.com>
2025-04-02 13:13:41 -0300

    Fix: Change the folder organization

--- scripts/login.py
+++ scripts/login.py
@@ -1,37 +1,76 @@
+import customtkinter as ctk
 
-import customtkinter as tk
+def Login(authData, UsersData):    
 
-root = tk.CTk()
-root.geometry(f'{root.winfo_screenwidth()}x{root.winfo_screenheight()}')
-root.title("aplicação")
+    def closeProgram():
+        if authData is not None:
+            authData[0] = {"login":False, "signUp":False, "repeat":False}
+            root.result = authData
+        root.quit()
+        root.destroy()
 
-# input do email
-email_label = tk.CTkLabel(root, text="email", font=('Arial',18), justify="left")
-email_label.pack(padx=20)
+    def findUser(Username, Password):
+        return next((user for user in UsersData if user['name'] == Username and user['password'] == Password), False)
 
-email_input = tk.CTkTextbox(root, font=('Arial',18), height=50)
-email_input.pack(padx=20)
+    def VerifyLogin():
+        Username = nome_input.get()
+        Password = senha_input.get()
+        User = findUser(Username, Password)
 
-# input da senha
-senha_label = tk.CTkLabel(root, text="senha", font=('Arial',18),  justify="left")
-senha_label.pack(padx=20)
+        if User:
+            authData[0] = {"login":False, "signUp":False, "repeat":False}
+            authData[1] = User
+            root.result = authData
+            root.quit()
+            root.destroy()
+        else: 
+            print("usuario ou senha invalido")
 
-senha_input = tk.CTkTextbox(root, font=('Arial',18), height=50)
-senha_input.pack(padx=20)
+    def goToSignUp():
+        authData[0] = {"login":False, "signUp":True, "repeat":True}
+        root.result = authData
+        root.quit()
+        root.destroy()
 
-# frame dos buttons
-buttonframe = tk.CTkFrame(root)
-buttonframe.columnconfigure(0, weight=1)
-buttonframe.columnconfigure(1, weight=1)
-buttonframe.pack(pady=20)
+    root = ctk.CTk()
+    root.geometry('320x350')
+    root.title("aplicação")
+    root.protocol("WM_DELETE_WINDOW", closeProgram)
 
-# button enviar
-btn_send = tk.CTkButton(buttonframe, text="enviar")
-btn_send.grid(row=0, column=0, sticky=tk.W+tk.E)
+    ctk.CTkLabel(root, text="Conecte-se", font=('arial', 18)).pack(pady=10)
 
-# button cancelar
-btn_cancel = tk.CTkButton(buttonframe, text="cancelar")
-btn_cancel.grid(row=0, column=1, sticky=tk.W+tk.E)
+    # input container
+    Input_Container = ctk.CTkFrame(root, fg_color='transparent')
+    Input_Container.columnconfigure(0, minsize=60)
+    Input_Container.columnconfigure(1, minsize=150)
+    Input_Container.pack(pady=35, padx=10, fill='both', expand=True)
 
-buttonframe.pack()
-root.mainloop()
+    # input do email
+    ctk.CTkLabel(Input_Container, text="Usuário: ", font=('Arial',12)).grid(row=0, column=0, pady=10, sticky="w")
+    nome_input = ctk.CTkEntry(Input_Container, font=('Arial',13), placeholder_text="Digite o seu nome de usuário", width=230)
+    nome_input.grid(row=0, column=1, pady=10, sticky="w")
+
+    # input da senha
+    ctk.CTkLabel(Input_Container, text="Senha:", font=('Arial',12)).grid(row=1, column=0, pady=10, sticky="w")
+    senha_input = ctk.CTkEntry(Input_Container, font=('Arial',13), placeholder_text="Digite a sua senha", width=230)
+    senha_input.grid(row=1, column=1, pady=10, sticky="w")
+    
+    # frame dos buttons
+    buttonframe = ctk.CTkFrame(root, fg_color="transparent")
+    buttonframe.columnconfigure(0, weight=1)
+    buttonframe.columnconfigure(1, weight=1)
+    buttonframe.pack(pady=10)
+    
+    # button cancelar
+    btn_cancel = ctk.CTkButton(buttonframe, text="cancelar", command=closeProgram, fg_color="#606060", hover_color="#777676", font=('Arial',12))
+    btn_cancel.grid(row=0, column=0, padx=10)
+    
+    # button enviar
+    btn_send = ctk.CTkButton(buttonframe, text="enviar", command=VerifyLogin, fg_color="#025604", hover_color="#027506", font=('Arial',12))
+    btn_send.grid(row=0, column=1, padx=10)
+    
+    ctk.CTkButton(root, text="Criar uma conta.", command=goToSignUp, fg_color="transparent", hover_color="#4158D0", text_color="white", corner_radius=8).pack(pady=20)
+
+    root.mainloop()
+
+    return root.result
